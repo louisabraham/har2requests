63d6cad4f55b03f8b74937634d24452e5a94ce82
louisabraham <louis.abraham@yahoo.fr>
2019-04-03 21:44:17 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="har2requests",
-    version="0.0.1",
+    version="0.0.2",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
