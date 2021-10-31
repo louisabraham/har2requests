347f6ccca4d80c53bbfc5ef916609cd4e77f1440
Louis Abraham <louis.abraham@yahoo.fr>
2021-10-31 16:18:59 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="har2requests",
-    version="0.2.1",
+    version="0.2.2",
     author="Louis Abraham",
     license="MIT",
     author_email="louis.abraham@yahoo.fr",
